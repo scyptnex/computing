b9302d30764724b93f6b78321fe79213b650069e
Scyptnex <scyptnex@Tzeidn.(none)>
2012-10-01 19:10:31 +1000

    finished transfer protocol

--- SecureFileSystem/FileServer.java
+++ SecureFileSystem/FileServer.java
@@ -19,7 +19,7 @@ public class FileServer {
 		if(args.length == 0){
 			ServerSocket ss = new ServerSocket(DEFAULT_PORT);
 			Socket sck = ss.accept();
-			File fi = new File("sample.txt");
+			File fi = new File("ncd1.tar.gz");
 			byte[] hsh = SecureUtils.hash(fi);
 			System.out.println(SecureUtils.hexify(hsh));
 			sendFile(sck, fi, hsh);
@@ -27,7 +27,7 @@ public class FileServer {
 		}
 		else{
 			Socket sck = new Socket("localhost", DEFAULT_PORT);
-			byte[] hsh = recvFile(sck, new File("transfer.out"));
+			byte[] hsh = recvFile(sck, new File("transfer.tar.gz"));
 			System.out.println(SecureUtils.hexify(hsh));
 			sck.close();
 		}
@@ -40,7 +40,6 @@ public class FileServer {
 		int block = 0;
 		FileInputStream fis = new FileInputStream(in);
 		while(block*BLOCK_SIZE < len){//while the client is waiting for the file
-			MessageDigest minor = MessageDigest.getInstance(SecureUtils.STD_HASH);
 			int blockSize = (int)Math.min(len-block*BLOCK_SIZE, BLOCK_SIZE);
 			writeInteger(sock.getOutputStream(), blockSize);//send the size of this block
 			byte[] minorHash = hashpipe(fis, sock.getOutputStream(), blockSize);//send the contents of the block
@@ -50,7 +49,7 @@ public class FileServer {
 			}
 			else{//if the client's hash was incorrect
 				writeInteger(sock.getOutputStream(), TRANS_ERROR);//tell the client it misread that last block
-				fis = resetInput(fis, block*BLOCK_SIZE);//reset the file reader to the start of the current block
+				fis = resetInput(fis, in, block*BLOCK_SIZE);//reset the file reader to the start of the current block
 			}
 			if(block*BLOCK_SIZE >= len){//if this is the last block
 				writeInteger(sock.getOutputStream(), TRANS_FINISH);//send the end signal
@@ -62,53 +61,61 @@ public class FileServer {
 					//ive no idea how we could have gotten to here
 					writeInteger(sock.getOutputStream(), TRANS_RESTART);//send the restart signal
 					block = 0;//set the block to be 0
-					fis = resetInput(fis, 0);//reset the file reader to the start of the current block
+					fis = resetInput(fis, in, 0);//reset the file reader to the start of the current block
 				}
 			}
 		}
 	}
 	
 	public static byte[] recvFile(Socket sock, File out) throws IOException, NoSuchAlgorithmException{
-		File tempOut = new File(out.getParentFile(), out.getName() + ".part");
 		FileOutputStream mainOut = new FileOutputStream(out);
 		byte[] ret = null;
 		boolean reading = true;
+		MessageDigest major = MessageDigest.getInstance(SecureUtils.STD_HASH);
+		int block = 0;
 		while(reading){//while there is data to be read
 			long readln = readInteger(sock.getInputStream());//read the length of the next block
 			if(readln == TRANS_FINISH){//if there are no more block
 				mainOut.close();
-				sock.getOutputStream().write(major.digest());//send my major hash
+				ret = major.digest();
+				sock.getOutputStream().write(ret);//send my major hash
 				long finishReply = readInteger(sock.getInputStream());//read the reply
 				if(finishReply == TRANS_FINISH){//if the major hash was correct
 					reading = false;//complete the transaction
 				}
 				else{//else the major has was incorrect
-					tempOut.delete();
-					out.delete();//delete both files
+					out.delete();//delete the file
 					mainOut = new FileOutputStream(out);//reset the output streams
 					//start again
+					block = 0;
+					major = MessageDigest.getInstance(SecureUtils.STD_HASH);
+					ret = null;
 				}
 			}
 			else{//else there are more blocks
-				FileOutputStream partStream = new FileOutputStream(tempOut);
-				hashpipe(sock.getInputStream(), partStream, readln);//read the block to temp file
-				//send my minor hash
-				//read the reply
-				if(true){//if the transfer was correct
-					//send the temp file to the main file
-					//increment the block
+				File tempOut = new File(out.getParentFile(), out.getName() + ".part" + block);
+				FileOutputStream partStream = new FileOutputStream(tempOut);//spawn a temporary file
+				byte[] hash = hashpipe(sock.getInputStream(), partStream, readln);//read the block to temp file
+				partStream.close();
+				sock.getOutputStream().write(hash);//send my minor hash
+				long minorReply = readInteger(sock.getInputStream());//read the reply
+				if(minorReply == TRANS_SUCCESS){//if the transfer was correct
+					FileInputStream partIn = new FileInputStream(tempOut);
+					hashpipe(partIn, mainOut, readln, major);//send the temp file to the main file
+					block++;
 				}
 				else{//if the transfer was wrong
-					//delete the temp file
+					//do nothing
 				}
+				tempOut.delete();//delete the temp file
 			}
 		}
 		return ret;
 	}
 	
-	public static FileInputStream resetInput(FileInputStream stream, long toPoint) throws IOException{
+	public static FileInputStream resetInput(FileInputStream stream, File in, long toPoint) throws IOException{
 		stream.close();
-		FileInputStream ret = new FileInputStream(stream.getFD());
+		FileInputStream ret = new FileInputStream(in);
 		ret.skip(toPoint);
 		return ret;
 	}
@@ -122,16 +129,19 @@ public class FileServer {
 	}
 	
 	public static byte[] hashpipe(InputStream is, OutputStream os, long len) throws IOException, NoSuchAlgorithmException{
+		MessageDigest md = MessageDigest.getInstance(SecureUtils.STD_HASH);
+		hashpipe(is, os, len, md);
+		return md.digest();
+	}
+	public static void hashpipe(InputStream is, OutputStream os, long len, MessageDigest md) throws IOException{
 		byte[] buf = new byte[SMALL_BUFF];
 		long read = 0;
-		MessageDigest md = MessageDigest.getInstance(SecureUtils.STD_HASH);
 		while(read < len){
 			int thisRead = is.read(buf, 0, (int)Math.min(SMALL_BUFF, len-read));
 			read += thisRead;
 			os.write(buf, 0, thisRead);
 			md.update(buf, 0, thisRead);
 		}
-		return md.digest();
 	}
 
 	//wonder if throwing error on end of stream fucks over other implementations? :P
