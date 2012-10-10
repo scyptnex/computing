5f3bc619c622ec287e6ee1d63a83c1f43568973d
Scyptnex <scyptnex@Ghesseran.(none)>
2012-10-10 12:17:57 +1100

    not working well on linux

--- NetworkEngine/SharedState.java
+++ NetworkEngine/SharedState.java
@@ -1,38 +1,76 @@
 import java.util.*;
 
-public class SharedState {
+public abstract class SharedState extends Thread{
+
+	private final ArrayList<Integer> ord;
+	private final ArrayList<Integer> code;
+	private final ArrayList<String> desc;
+	private final ArrayList<Double> val;
+	private final ArrayList<Double> d1;
+	private final ArrayList<Double> d2;
+	private int valCounter;
+	private final int msPerTick;
+	private final int ticksPerUpdate;
+	private long totalTicks;
+	private boolean runningLoop;
 	
-	//statics
-	private final ArrayList<Long> sNum;
-	private final ArrayList<String> sText;
-	private final ArrayList<byte[]> sData;
-	
-	//dynamics
-	
-	
-	public SharedState(int mss, int mds){
-		sNum = new ArrayList<Long>();
-		sText = new ArrayList<String>();
-		sData = new ArrayList<byte[]>();
+	public SharedState(int tps, int tpu){
+		valCounter = 0;
+		ord = new ArrayList<Integer>();
+		code = new ArrayList<Integer>();
+		desc = new ArrayList<String>();
+		val = new ArrayList<Double>();
+		d1 = new ArrayList<Double>();
+		d2 = new ArrayList<Double>();
+		msPerTick = 1000/tps;
+		ticksPerUpdate = tpu;
+		runningLoop = false;
 	}
 	
-	public int addStaticNum(long n){
-		sNum.add(n);
-		return sNum.size()-1;
+	private void networkUpdate(){
+		
 	}
 	
-	public int addStaticNum(int i){
-		return addStaticNum(i);
+	public final int init(double startval, int valType, String description){
+		return init(startval, 0, 0, valType, description);
+	}
+	public final int init(double startval, double firstDiff, double secondDiff, int valType, String description){
+		int ret = valCounter;
+		ord.add(ret);
+		code.add(valType);
+		desc.add(description);
+		val.add(startval);
+		d1.add(firstDiff);
+		d2.add(secondDiff);
+		valCounter++;
+		return ret;
 	}
 	
-	public int addStaticText(String s){
-		sText.add(s);
-		return sNum.size()-1;
-	}
+	public abstract void tick();
+	public abstract void render();
 	
-	public int addStaticData(byte[] d){
-		sData.add(d);
-		return sData.size()-1;
+	public void stopLoop(){
+		runningLoop = false;
 	}
-	
+	public void startLoop(){
+		this.start();
+	}
+	public void run(){
+		long lastLoop = System.currentTimeMillis();
+		runningLoop = true;
+		totalTicks = 0;
+		while(runningLoop){
+			long tloop = System.currentTimeMillis();
+			long tdiff = tloop - lastLoop;
+			int numTicks = (int)tdiff/msPerTick;
+			lastLoop = lastLoop + msPerTick*numTicks;
+			for(int t=0; t<totalTicks; t++){
+				if(totalTicks%ticksPerUpdate == 0) networkUpdate();
+				tick();
+				totalTicks++;
+			}
+			render();
+			Thread.yield();
+		}
+	}
 }
--- NetworkEngine/Zssr.java
+++ NetworkEngine/Zssr.java
@@ -2,16 +2,20 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.*;
 
 public class Zssr extends JComponent{
 	
-	public final SharedState state;
+	public NetState ns;
+	
+	ArrayList<Integer> xobjs;
+	ArrayList<Integer> yobjs;
 	
 	public static void main(String[] args){
 		JFrame frm = new JFrame();
 		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Zssr rend = new Zssr(new SharedState(1024, 1024));
+		Zssr rend = new Zssr();
 		
 		Container c = frm.getContentPane();
 		c.setLayout(new BorderLayout());
@@ -20,10 +24,10 @@ public class Zssr extends JComponent{
 		
 		frm.pack();
 		frm.setVisible(true);
+		rend.ns.start();
 	}
 	
-	public Zssr(SharedState ss){
-		state = ss;
+	public Zssr(){
 		this.addMouseListener(new MouseListener(){
 			public void mouseClicked(MouseEvent arg0) {}
 			public void mouseEntered(MouseEvent arg0) {}
@@ -33,6 +37,17 @@ public class Zssr extends JComponent{
 			}
 			public void mouseReleased(MouseEvent arg0) {}
 		});
+		
+		ns = new NetState(50, 5, this);
+	}
+	
+	public void paint(Graphics g){
+		Graphics2D g2 = (Graphics2D)g;
+		g2.drawRect(50, 50, 100, 100);
+	}
+	
+	public void advance(){
+		System.out.println("wheee");
 	}
 	
 	public Dimension getPreferredSize(){
@@ -48,7 +63,34 @@ public class Zssr extends JComponent{
 	}
 	
 	public void click(int x, int y, int b){
+		if(b == MouseEvent.BUTTON3){//right click
+			xobjs.add(ns.init(x, 0, "x coord"));
+			yobjs.add(ns.init(y, 0, "x coord"));
+		}
+		else{//'left' click
+			
+		}
 		System.out.println(x + ", " + y + " = " + b);
+	}
+	
+	public class NetState extends SharedState{
+		
+		public final JComponent parent;
+
+		public NetState(int tps, int tpu, JComponent comp) {
+			super(tps, tpu);
+			parent = comp;
+		}
+		
+		public void tick() {
+			advance();
+		}
+
+		@Override
+		public void render() {
+			parent.repaint();
+		}
+		
 	}
 	
 }
