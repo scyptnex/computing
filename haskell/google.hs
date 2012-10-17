f33514c36a7310a4e0c12cef6fcc457e6f78d970
Scyptnex <scyptnex@Ghesseran.(none)>
2012-10-17 15:35:43 +1100

    some wacky networked haskell examples

---
+++ haskell/google.hs
@@ -0,0 +1,10 @@
+import Network
+import System.IO
+ 
+main = withSocketsDo $ do
+    h <- connectTo "www.google.com" (PortNumber 80)
+    hSetBuffering h LineBuffering
+    hPutStr h "GET / HTTP/1.1\nhost: www.google.com\n\n"
+    contents <- hGetContents h
+    putStrLn contents
+    hClose h
