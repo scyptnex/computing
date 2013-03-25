8b2cc296f1813797c0ac550c37de9799d47a2ed9
Scyptnex <scyptnex@Ghesseran.(none)>
2013-03-25 15:41:56 +1100

    beginning of colony

---
+++ Colony/Site.java
@@ -0,0 +1,80 @@
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class Site {
+	
+	public static final int COARSE_WIDTH = 16;
+	public static final int COARSE_HEIGHT = 16;
+
+	public static final int SUBSITE_Y = COARSE_WIDTH*COARSE_HEIGHT;//down-up
+	public static final int SUBSITE_X = SUBSITE_Y;//west-east
+	public static final int SUBSITE_Z = SUBSITE_Y;//south-north
+	
+	private final int subsiteX;
+	private final int subsiteY;
+	private final int subsiteZ;
+	private BufferedImage[][][] siteContents;
+	
+	public static void main(String[] args){
+		Site s = new Site(1,1,1);
+		for(int x=0; x<256; x++){
+			for(int y=0; y<256; y++){
+				for(int z=0; z<256; z++){
+					s.set(x, y, z, (255<<24));
+					if(x > 15 && x < 64 && z > 112 && z < 140 && y > 3 && y < 33){
+						s.set(x, y, z, s.get(x, y, z)|255);
+					}
+				}
+			}
+		}
+		s.save(new File("."));
+	}
+	
+	public Site(int xLen, int yLen, int zLen){
+		subsiteX = xLen;
+		subsiteY = yLen;
+		subsiteZ = zLen;
+		siteContents = new BufferedImage[subsiteX][subsiteY][subsiteZ];
+		for(int x=0; x<subsiteX; x++) for(int y=0; y<subsiteY; y++) for (int z=0; z<subsiteZ; z++){
+			siteContents[x][y][z] = new BufferedImage(COARSE_WIDTH*SUBSITE_X, COARSE_HEIGHT*SUBSITE_Z, BufferedImage.TYPE_INT_ARGB);
+		}
+	}
+	
+	public boolean save(File dirLoc){
+		if(!dirLoc.isDirectory()) return false;
+		try{
+			for(int x=0; x<subsiteX; x++) for(int y=0; y<subsiteY; y++) for (int z=0; z<subsiteZ; z++){
+				File write = new File(dirLoc, "site-"+x+"-"+y+"-"+z+".png");
+				ImageIO.write(siteContents[x][y][z], "png", write);
+			}
+		}
+		catch(IOException exc){
+			return false;
+		}
+		return true;
+	}
+	
+	public void set(int x, int y, int z, int val){
+		BufferedImage subsite = siteContents[x/SUBSITE_X][y/SUBSITE_Y][z/SUBSITE_Z];
+		x %= SUBSITE_X;
+		y %= SUBSITE_Y;
+		z %= SUBSITE_Z;
+		//CROSS PIXEL is Y, thanks minecraft
+		int heightGridWidth = y%COARSE_WIDTH;
+		int heightGridHeight = y/COARSE_WIDTH;
+		subsite.setRGB(heightGridWidth*SUBSITE_X + x, heightGridHeight*SUBSITE_Z + z, val);
+	}
+	
+	public int get(int x, int y, int z){
+		BufferedImage subsite = siteContents[x/SUBSITE_X][y/SUBSITE_Y][z/SUBSITE_Z];
+		x %= SUBSITE_X;
+		y %= SUBSITE_Y;
+		z %= SUBSITE_Z;
+		//CROSS PIXEL is Y, thanks minecraft
+		int heightGridWidth = y%COARSE_WIDTH;
+		int heightGridHeight = y/COARSE_WIDTH;
+		return subsite.getRGB(heightGridWidth*SUBSITE_X + x, heightGridHeight*SUBSITE_Z + z);
+	}
+	
+}
