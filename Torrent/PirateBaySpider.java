042e1cab498135d7d24478407293f636642ad379
Scyptnex <scyptnex@hotmail.com>
2014-03-02 14:58:01 +1000

    small work on spider using jsoup

--- Torrent/PirateBaySpider.java
+++ Torrent/PirateBaySpider.java
@@ -3,16 +3,31 @@ import java.net.URL;
 
 import scyp.net.*;
 
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
 public class PirateBaySpider {
 
 	private String searchString = null;
 
 	public static void main(String[] args) throws IOException{
 		PirateBaySpider pbs = new PirateBaySpider("black sails s01e04");
-		System.out.println(pbs.getSearchPage(1));
-		for(String s : Unstream.toLineIterable(WUtil.streamURL(pbs.getSearchPage(0), true))){
-			System.out.println(s);
+		Document doc = Jsoup.connect(pbs.getSearchPage(0).toString()).get();
+		Elements e = doc.getElementsByTag("tr");
+		for(Element elem : e){
+			Elements tdatas = elem.children();
+			System.out.println(tdatas.get(1).text());
 		}
+		System.out.println(doc.title());
+		System.out.println(e.size());
+		BufferedWriter htmlWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("tst.html"), "UTF-8"));
+		htmlWriter.write(doc.toString());
+		htmlWriter.close();
+		//System.out.println(pbs.getSearchPage(1));
+		//for(String s : Unstream.toLineIterable(WUtil.streamURL(pbs.getSearchPage(0), true))){
+		//	System.out.println(s);
+		//}
 		
 	}
 
