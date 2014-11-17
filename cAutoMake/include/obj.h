0a506e7892b66040f39ae42400ea6b9792f89c87
Scyptnex <Tzeidn 14>
2014-11-17 13:37:43 +1000

    Beginning automake stuff

---
+++ cAutoMake/include/obj.h
@@ -0,0 +1,12 @@
+#ifndef __OBJ_H__
+#define __OBJ_H__
+
+class Obj{
+    private:
+        int val;
+    public:
+        Obj(int v);
+        int get();
+};
+
+#endif //__OBJ_H__
---
+++ cAutoMake/src/obj.cc
@@ -0,0 +1,7 @@
+#include "obj.h"
+
+Obj::Obj(int v) : val(v) {}
+
+int Obj::get(){
+    return val;
+}
