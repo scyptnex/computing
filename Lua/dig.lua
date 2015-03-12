3f52d81a47e034f9dadccdd4d956132a3d236370
Scyptnex <scyptnex@hotmail.com>
2015-03-12 22:00:19 +1000

    multi-directional dig

--- Lua/dig.lua
+++ Lua/dig.lua
@@ -7,13 +7,21 @@ end
 
 local tArgs = {...}
 local forward = tonumber(tArgs[1])
+local right = tonumber(tArgs[2])
 if turtle.getFuelLevel() < forward then
 	print("Out of fuel")
 	exit();
 end
-print("Digging forward " .. forward)
-for f=1,forward do
-	while turtle.detectUp() do turtle.digUp() end
-	while turtle.detectDown() do turtle.digDown() end
-	if f ~= forward then goF() end
+print("Digging forward " .. forward .. " right " .. right)
+for r=1,right do
+	for f=1,forward do
+		while turtle.detectUp() do turtle.digUp() end
+		while turtle.detectDown() do turtle.digDown() end
+		if f ~= forward then goF() end
+	end
+	if r ~= right then
+		if r%2 == 1 then turtle.turnRight() else turtle.turnLeft() end
+		goF()
+		if r%2 == 1 then turtle.turnRight() else turtle.turnLeft() end
+	end
 end
