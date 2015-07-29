d8e02758a45eecca516501a6b397e7e1c6afbf16
Scyptnex <scyptnex@tzeidn.com>
2015-07-29 14:47:35 +1000

    So much prime fixing, finally have a nice prime factory

---
+++ Euler/p041t080/Euler070TotientPerm.java
@@ -0,0 +1,7 @@
+package p041t080;
+
+/**
+ * Created by nic on 28/07/15.
+ */
+public class Euler070TotientPerm {
+}
---
+++ Euler/util/PrimeFactory.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by nic on 29/07/15.
+ */
+public class PrimeFactory {
+}
