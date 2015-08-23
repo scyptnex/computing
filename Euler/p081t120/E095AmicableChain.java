3b2a77122de4ae5cfc1b2e1ce7d2973068e231a1
Scyptnex <scyptnex@hotmail.com>
2015-08-23 14:44:26 +1000

    finished simple eul

--- Euler/p081t120/E095AmicableChain.java
+++ Euler/p081t120/E095AmicableChain.java
@@ -1,35 +1,48 @@
 package p081t120;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import util.Numeral;
 import util.PrimeFactory;
-import util.Primer;
-
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class E095AmicableChain {
 
     public static final int[] cs = new int[100];
 
     public static void main(String[] args){
-        LongStream.range(3, 100)
-                .mapToInt(E095AmicableChain::clen)
-                .peek(System.out::println)
-                .boxed()
-                .collect(Collectors.toList());
+    	int max = 0;
+    	int maxNum = -1;
+    	for(int i=3; i<1000001; i++){
+    		//System.out.println(Numeral.divisors(i));
+    		int cur = clen(i);
+    		if(cur > max){
+    			max = cur;
+    			maxNum = i;
+    		}
+    	}
+    	System.out.println(maxNum);
     }
 
 
     public static int clen(long num){
         int count = 0;
         long cur = num;
-        while(true){
+        Set<Long> seen = new HashSet<>();
+        do{
+        	seen.add(cur);
+        	if(cur > 1000000){
+        		count = 0;
+        		break;
+        	}
             cur = Numeral.divisors(cur).stream().mapToLong(Long::longValue).sum() - cur;
-            if(cur == num) return count;
-            if(cur > 1000000) return 0;
             count++;
+        } while(!seen.contains(cur));
+        if(cur == num){
+        	System.out.println(num + " " + count + " - " + seen);
+        	return count;
         }
+        return 0;
     }
 
 }
