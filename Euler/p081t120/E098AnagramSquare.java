fd0cb3706fbc051f14e239fb31de58fabd3a4cf2
Scyptnex <scyptnex@hotmail.com>
2015-09-09 22:46:08 +1000

    keep on eulering

--- Euler/p081t120/E098AnagramSquare.java
+++ Euler/p081t120/E098AnagramSquare.java
@@ -7,10 +7,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class E098AnagramSquare {
@@ -37,8 +41,41 @@ public class E098AnagramSquare {
                 .collect(Collectors.toList());
         anagramPairs.clear();
         anagramPairs.add(new Collect.Pair<>("CARE", "RACE"));
-        anagramPairs.stream().parallel()
-                .map(p -> new Collect.Pair<>((p.first+p.second).chars().sorted().distinct().mapToObj(i -> "" + (char)i).collect(Collectors.joining()), p))
-                .forEach(System.out::println);
+        anagramPairs.stream().flatMap(E098AnagramSquare::conv).forEach(System.out::println);
+//        int largestDigits=anagramPairs.stream()
+//        		.mapToInt(p -> p.first.length())
+//        		.max().orElse(0);
+//        int smallestDigits=anagramPairs.stream()
+//        		.mapToInt(p -> p.first.length())
+//        		.min().orElse(-1);
+//        anagramPairs.stream().parallel()
+//                .map(p -> new Collect.Pair<>((p.first+p.second).chars().sorted().distinct().mapToObj(i -> "" + (char)i).collect(Collectors.joining()), p))
+//                .forEach(System.out::println);
+//        Map<Integer, Set<Integer>> grps = IntStream.rangeClosed(smallestDigits, largestDigits).collect(() -> new HashMap<Integer, Set<Integer>>(), (m,i)->m.put(i, new HashSet<Integer>()), (a,b) -> a.putAll(b));
+    }
+    
+    public static HashMap<Integer, Set<Integer>> convCache = new HashMap<>();
+    public static Stream<Collect.Pair<Integer, Integer>> conv(final Collect.Pair<String, String> in){
+    	int l = in.first.length();
+    	if(!convCache.containsKey(l)){
+    		convCache.put(l, IntStream
+    				.rangeClosed((int)Math.ceil(Math.sqrt(Math.pow(10, l-1))), (int)Math.floor(Math.sqrt(Math.pow(10, l))))
+    				.map(i -> i*i).boxed().collect(Collectors.toSet()));
+    	}
+    	return convCache.get(l).stream().parallel()
+    			.map(i -> );
+    }
+    
+    public static char[] digitMap(String wrd, int num, int len){
+    	char[] ret = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    	int i=1;
+    	while(num > 0){
+    		int idx = num%10;
+    		char c = wrd.charAt(len-i);
+    		if(ret[idx] == ' ')
+    		num = num/10;
+    		i++;
+    	}
+    	return ret;
     }
 }
