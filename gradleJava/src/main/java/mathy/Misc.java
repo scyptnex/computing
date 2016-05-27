d80eed53dca7011580e516097d1c7de37dcea410
Scyptnex <nic.h.misc@gmail.com>
2016-05-27 11:09:22 +1000

    antlr parsing in the java gradle example

--- gradleJava/src/main/java/mathy/Misc.java
+++ gradleJava/src/main/java/mathy/Misc.java
@@ -1,16 +1,33 @@
 package mathy;
 
+import exprs.ExprLexer;
+import exprs.ExprParser;
+import org.antlr.v4.runtime.*;
+
 public class Misc {
+
     public static long fibo(int n){
         if(n < 2) return 1;
         return fibo(n-1) + fibo(n-2);
     }
+
+    public static void parse(String expr){
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(expr);
+        Lexer lexer = new ExprLexer(antlrInputStream);
+        TokenStream tokenStream = new CommonTokenStream(lexer);
+        ExprParser parser = new ExprParser(tokenStream);
+        ExprParser.ProgContext tree = parser.prog();
+        System.out.println(tree.toStringTree(parser));
+    }
+
     public static void main(String[] args){
         try{
             int num = Integer.parseInt(args[0]);
             System.out.println("Fib(" + num + ") = " + fibo(num) + ".");
         } catch(Exception e){
             System.err.println("Usage: java mathy.Misc <number>");
-        } 
+        }
+
+        parse("x = 5\n y = 3 \n");
     }
 }
---
+++ gradleJava/src/test/java/mathy/TestExpr.java
@@ -0,0 +1,17 @@
+package mathy;
+
+import org.junit.Test;
+
+/**
+ * TestExpr
+ * <p/>
+ * Author: nic
+ * Date: 27/05/16
+ */
+public class TestExpr {
+
+    @Test
+    public void testShortExpr(){
+    }
+
+}
