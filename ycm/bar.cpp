585dca3d6aaf4272c6e111959bca13cdaacd1c91
Scyptnex <nic.h.misc@gmail.com>
2016-06-20 13:40:10 +1000

    a small ycm example

---
+++ ycm/bar.cpp
@@ -0,0 +1,18 @@
+/************************************************************************* 
+ *                                bar.cpp                                *
+ *                                                                       *
+ * Author: Nic H.                                                        *
+ * Date: 2016-Jun-20                                                     *
+ *************************************************************************/
+
+#include "bar.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "bar" << endl;
+    cout << bar::b1() << endl;
+    cout << foo::f1() << endl;
+}
+
