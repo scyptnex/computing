c3cb47ae5eea0699aa52b4e3fb8d9f7c4bc15603
Scyptnex <nic.h.misc@gmail.com>
2016-06-21 16:14:41 +1000

    cmake tests

--- cmake/testing/main.cpp
+++ cmake/testing/main.cpp
@@ -1,5 +1,17 @@
 #include <cstdio>
 int main(int argc, char** argv){
     printf("%d arguments\n", argc);
-    return argc < 2;
+    for(int i=0; i<argc; i++){
+        printf(" - %s\n", argv[i]);
+    }
+    if(argc > 1){
+        char c = argv[1][0];
+        if(c <= '9' && c >= '0'){
+            return c - '0';
+        } else {
+            return 1;
+        }
+    } else {
+        return 0;
+    }
 }
