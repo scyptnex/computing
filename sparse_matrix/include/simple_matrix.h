fd74830e0ae1f46984d8fbae8619b77894302bb1
Scyptnex <nic.h.misc@gmail.com>
2016-06-23 17:03:30 +1000

    matrices wont multiply

--- sparse_matrix/include/simple_matrix.h
+++ sparse_matrix/include/simple_matrix.h
@@ -25,6 +25,18 @@ class simple_matrix : public matrix<R, C, simple_matrix> {
         void set(unsigned r, unsigned c) { mat[c*R + r] = true; }
         void unset(unsigned r, unsigned c) { mat[c*R + r] = false; }
         bool get(unsigned r, unsigned c) const { return mat[c*R + r]; }
+        template<unsigned X>
+        simple_matrix<R,X> operator*(const simple_matrix<C, X>& other){
+            simple_matrix<R, X> ret;
+            for(int r=0; r<R; r++){
+                for(int x=0; x<X; x++){
+                    for(int c=0; c<C; c++){
+                        if(get(r, c) && other.get(c, x)) ret.set(r, x);
+                    }
+                }
+            }
+            return ret;
+        }
 };
 
 #endif /* __SIMPLE_MATRIX_H__ */
--- sparse_matrix/test/simple_matrix.cpp
+++ sparse_matrix/test/simple_matrix.cpp
@@ -8,7 +8,7 @@
 
 BOOST_AUTO_TEST_SUITE( test_simple_matrix )
 
-    BOOST_AUTO_TEST_CASE ( f2 ){
+    BOOST_AUTO_TEST_CASE ( basic ){
         simple_matrix<2,2> sm;
         BOOST_CHECK(sm.rows == 2);
         BOOST_CHECK(sm.cols == 2);
@@ -24,6 +24,23 @@ BOOST_AUTO_TEST_SUITE( test_simple_matrix )
         std::stringstream ss2;
         sm.draw(ss2);
         BOOST_CHECK(ss2.str() == "1 0\n1 0");
+    }
+
+    BOOST_AUTO_TEST_CASE ( mult ) {
+        simple_matrix<2, 2> m1;
+        m1.set(0,1);
+        simple_matrix<2, 2> m2;
+        m2.set(1,0);
+        auto m12 = m1*m2;
+        BOOST_CHECK( m12.get(0, 0));
+        BOOST_CHECK(!m12.get(0, 1));
+        BOOST_CHECK(!m12.get(1, 0));
+        BOOST_CHECK(!m12.get(1, 1));
+        auto m21 = m2*m1;
+        BOOST_CHECK(!m21.get(0, 0));
+        BOOST_CHECK(!m21.get(0, 1));
+        BOOST_CHECK(!m21.get(1, 0));
+        BOOST_CHECK( m21.get(1, 1));
     }
 
 BOOST_AUTO_TEST_SUITE_END()
