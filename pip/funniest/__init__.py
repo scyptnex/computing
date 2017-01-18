7cea5e204cb94fc6c64cccc44bbe04f65bd4b669
Scyptnex <nic.h.misc@gmail.com>
2017-01-19 10:21:58 +1100

    python packaging

---
+++ pip/funniest/__init__.py
@@ -0,0 +1,15 @@
+"""
++-------------------------------------------------------------------------+
+|                               __init__.py                               |
+|                                                                         |
+| Author: nic                                                             |
+| Date: 2017-Jan-19                                                       |
++-------------------------------------------------------------------------+
+"""
+
+__doc__ = __doc__.strip()
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
