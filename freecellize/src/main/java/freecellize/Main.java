7ee55d448088891158905a0182d40ae79e2de73e
Scyptnex <nic.h.misc@gmail.com>
2017-02-26 14:33:40 +1100

    freecellize project

---
+++ freecellize/src/main/java/freecellize/Main.java
@@ -0,0 +1,14 @@
+/************************************************************************* 
+ *                               Main.java                               *
+ *                                                                       *
+ * Author: nic                                                           *
+ * Date: 2017-Feb-26                                                     *
+ *************************************************************************/
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
